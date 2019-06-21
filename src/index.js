c5c30b8f19fb97bbe2933826358e3a8c5613c37f
sucksido <success.shibambu25@gmail.com>
2019-06-21 05:35:12 +0200

    changed default page
    changed default page

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 
-ReactDOM.render(<Home />, document.getElementById('root'));
+ReactDOM.render(<People />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
